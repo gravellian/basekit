91011860561d5c16ebd9842657fd0e7aa44145f0
Dana Gravelle <gravellecreative@gmail.com>
2025-11-15 12:48:46 -0800

    update subtheme script - avoid sed

--- subtheme-starter/scripts/new-subtheme.sh
+++ subtheme-starter/scripts/new-subtheme.sh
@@ -19,13 +19,13 @@ rsync -a --exclude 'scripts' --exclude '.git' "$STARTER_ROOT/" "$DEST_ROOT/"
 mv "$DEST_ROOT/__theme__.info.yml" "$DEST_ROOT/${THEME_MACHINE}.info.yml"
 mv "$DEST_ROOT/__theme__.libraries.yml" "$DEST_ROOT/${THEME_MACHINE}.libraries.yml"
 
-# Replace placeholders
-LC_ALL=C find "$DEST_ROOT" -type f -print0 | xargs -0 sed -i '' \
-  -e "s/__THEME_MACHINE__/${THEME_MACHINE}/g" \
-  -e "s/__THEME_NAME__/${THEME_NAME}/g"
+# Replace placeholders (BSD/macOS safe). Limit to text files and use Perl to avoid sed locale issues.
+export THEME_MACHINE THEME_NAME
+find "$DEST_ROOT" -type f \
+  \( -name "*.yml" -o -name "*.yaml" -o -name "*.scss" -o -name "*.css" -o -name "*.js" -o -name "*.json" -o -name "*.md" -o -name "*.info.yml" -o -name "*.libraries.yml" -o -name "gulpfile.js" -o -name "package.json" \) \
+  -print0 | xargs -0 perl -pi -e 's/__THEME_MACHINE__/$ENV{THEME_MACHINE}/g; s/__THEME_NAME__/$ENV{THEME_NAME}/g'
 
 echo "Scaffolded sub-theme at $DEST_ROOT"
 echo "Next steps:"
 echo "  cd $DEST_ROOT && npm install && npm run build"
 echo "  drush then ${THEME_MACHINE} -y && drush cset system.theme default ${THEME_MACHINE} -y && drush cr"
-
